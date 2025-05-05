fbe8f8ce3e4d0ce5fba2f51684f334cb231cb0ce
Мухаметзянов Ильназ <chizescrap@mail.ru>
2025-05-05 16:38:15 +0300

    added some validation to WorkTypePage

--- Kurs/Views/WorkTypePage.xaml.cs
+++ Kurs/Views/WorkTypePage.xaml.cs
@@ -6,4 +6,15 @@ public partial class WorkTypePage : ContentPage
     {
         InitializeComponent();
     }
+    private void OnRatePerDayTextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (sender is Entry entry)
+        {
+            // Разрешаем только цифры и запятую
+            string newText = new string(e.NewTextValue.Where(c => char.IsDigit(c) || c == ',' || c == '.').ToArray());
+
+            if (newText != e.NewTextValue)
+                entry.Text = newText;
+        }
+    }
 }
